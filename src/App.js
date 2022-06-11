aa64564f752766b72bacc100e7d62fd05eb37de8
Kabir Bolatito <tbola45@gmail.com>
2022-06-11 11:37:39 -0400

    Code initialization

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import Forecast from "./components/Forecast/Forecast";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>The Weather App</h1>
+      </header>
+      <main>
+        <Forecast />
+      </main>
+      <footer>
+        Made with ♥️ by Tito
+      </footer>
+    </div>
+  );
+}
+
+export default App;
